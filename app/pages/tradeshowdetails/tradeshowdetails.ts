8795aa734e749266519515e0dd5d1fe330952f57
Joel <jol_fakar@yahoo.co.id>
2017-01-06 10:08:04 +0800

    Update 06/01/2017 --JOEL

--- app/pages/tradeshowdetails/tradeshowdetails.ts
+++ app/pages/tradeshowdetails/tradeshowdetails.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController, AlertController, ModalController, ViewController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, AlertController, ModalController, ViewController, SqlStorage, Storage } from 'ionic-angular';
 import { Mrepcdata } from '../../providers/mrepcdata/mrepcdata';
 import { SupplierDetailsPage } from '../../pages/supplier-details/supplier-details';
+//import { Storage } from '@ionic/storage';
 
 let monthname = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 @Component({
-    templateUrl: 'build/pages/tradeshowdetails/tradeshowdetails.html',
+    templateUrl: 'build/pages/tradeshowdetails/tradeshowdetails.html'
 })
 export class TradeshowdetailsPage {
     geteventdetails: any;
@@ -16,6 +17,9 @@ export class TradeshowdetailsPage {
     eventdate: string;
     showBook: Boolean;
     participantdata: any;
+
+    private storage: Storage;
+    public personList: Array<Object>;
 
     constructor(
         private navCtrl: NavController,
@@ -42,11 +46,17 @@ export class TradeshowdetailsPage {
         } else {
             this.eventdate = this.eventstart.split(" ")[0] + " " + this.eventstart.split(" ")[1] + " to " + this.eventend;
         }
+
+
+        this.storage = new Storage(SqlStorage);
+        this.storage.query("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)");
+        this.personList = [];
     }
 
     onPageLoaded() {
         this.loadparticipantdata(this.navParams.get('eventid'));
         console.log(this.navParams.get('eventid'));
+        this.refresh();
     }
 
     convertdate(date){
@@ -83,6 +93,35 @@ export class TradeshowdetailsPage {
     registerform() {
         let modal = this.modalCtrl.create(TradeshowdetailsFormPage);
         modal.present();
+    }
+
+    additem() {
+        this.storage.query("INSERT INTO people (firstname, lastname) VALUES (?, ?)", ["Nic", "Raboy"]).then((data) => {
+            this.personList.push({
+                "firstname": "Nic",
+                "lastname": "Raboy"
+            });
+        }, (error) => {
+            console.log(error);
+        });
+    };
+
+    refresh() {
+        this.storage.query("SELECT * FROM people").then((data) => {
+            if(data.res.rows.length > 0) {
+                this.personList = [];
+                for(let i = 0; i < data.res.rows.length; i++) {
+                    this.personList.push({
+                        "id": data.res.rows.item(i).id,
+                        "firstname": data.res.rows.item(i).firstname,
+                        "lastname": data.res.rows.item(i).lastname,
+                    });
+                }
+                console.log(this.personList);
+            }
+        }, (error) => {
+            console.log(error);
+        });
     }
 
 }
